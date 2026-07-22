771fdff54184f5013b2b60764e019cb47cc1143e
NamrataSalgar427 <namratasalgar427@gmail.com>
2026-07-23 00:18:35 +0530

    Inital Landing Page Setup

---
+++ src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero'
+import Brokrage from './Brokerage';
+
+function PricingPage() {
+    return ( 
+    <>
+    <Hero/>
+    <Brokrage/>
+    </> );
+}
+
+export default PricingPage;
---
+++ src/landing_page/products/ProductsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './Hero';
+import Leftimage from './Leftimage';
+import Rightimage from './Rightimage';
+import Universe from './Universe';
+
+function ProductsPage() {
+    return ( 
+        <>
+        <Hero/>
+        <Leftimage/>
+        <Rightimage/>
+        <Universe/>
+        </>
+     );
+}
+
+export default ProductsPage;
